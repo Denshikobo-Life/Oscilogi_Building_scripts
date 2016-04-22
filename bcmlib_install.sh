2c930e1066682e95ee3fee1e87e7f060581ae159
Denshikobo-Life <hiramine0930@gmail.com>
2016-04-22 15:40:34 +0900

    New scripts
    Please read install_memo.txt
    And then,use proper scripts which you need.

---
+++ bcmlib_install.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/bash
+#<Install bcmlib_for_java>
+$ cd /opt/samba
+$ wget https://github.com/Denshikobo-Life/bcmlib_for_java/archive/master.zip
+$ unzip master.zip
+$ mv bcmlib_for_java-master bcmlib_for_java
+$ cd bcmlib_for_java
+$ make
+$ make install
+$ cd ./bcmlib_for_java/example
+$ chmod 755 *.sh
---
+++ oscilogi_install.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/bash
+#<Install Osci_logi_console>
+$ wget https://github.com/Denshikobo-Life/Oscilogi_Building_scripts/archive/master.zip
+$ unzip master.zip
+$ cd /opt/samba
+$ unzip Osci_logi_console.zip
+$ cp Osci_logi_console/dist/Osci_logi_console.jar bcmlib_for_java/examples/
+$ cp Osci_logi_console/dist/lib/AbsoluteLayout.jar bcmlib_for_java/examples/lib
+$ sudo cp bcmlib_for_java/examples/libbcm_interface.so /usr/local/lib
+$ sudo cp bcmlib_for_java/examples/libbcm_interface.so /usr/local/lib/libbcm_interface2.so
