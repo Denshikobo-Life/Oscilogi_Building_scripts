6962516a52f4c91a6761aedbef52e0e8aacf1e94
Denshikobo-Life <hiramine0930@gmail.com>
2016-04-23 14:13:25 +0900

    update

--- Osci_logi_console.sh
+++ Osci_logi_console.sh
@@ -4,7 +4,7 @@ if [ ! -f "/tmp/shm" ] ; then
      dd if=/dev/zero of=/tmp/shm bs=4096 count=1
 fi
 
-cp /opt/samba/netbeans_project/Osci_logi_console/dist/Osci_logi_console.jar ./
+cp /opt/samba/Osci_logi_console/dist/Osci_logi_console.jar ./
 java -cp ./:lib/jna.jar:lib/jna-platform.jar:lib/AbsoluteLayout.jar:dkl/bcm2835.class: -jar Osci_logi_console.jar /tmp/shm
 pkill bcm2835
 
---
+++ setup_icon.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Setup Oscilogi icon
+cp Oscilogi.png /opt/samba
+cp Start_Oscilofi410.sh ~/
+cp OsciLogi.desktop ~/Desktop
+
